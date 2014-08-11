56c3510bab76983d64e00d902db96d53c1851ff4
Julian Poyourow <julian@Julians-MacBook-Pro.local>
2014-08-11 14:05:16 -0700

    Installed angular

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name pointmapApp
+ * @description
+ * # pointmapApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('pointmapApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pointmapApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pointmapApp
+ */
+angular.module('pointmapApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

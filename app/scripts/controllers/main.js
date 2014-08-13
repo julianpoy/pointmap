1f3694d992393f257e032c4b18a84704ba729328
Julian Poyourow <julianpoyo@gmail.com>
2014-08-13 14:35:20 -0700

    Cleaned up main js

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,11 +9,6 @@
  */
 angular.module('pointmapApp')
   .controller('MainCtrl', function ($scope, Points) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
 
     $scope.map = {
 	    center: {
